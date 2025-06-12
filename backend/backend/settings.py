66048aa0fdd318bcc5eca97595b9f50549f83b60
ron <lucifron28@gmail.com>
2025-06-12 21:27:31 +0800

    added default permission classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,7 +140,10 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
