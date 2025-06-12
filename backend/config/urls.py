71034afea1d69e01a6b602d5c17d826eaf964a41
ron <lucifron28@gmail.com>
2025-06-12 20:45:43 +0800

    added names and jwt auth

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,26 +1,16 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/notes/', include('apps.notes.urls')),
-    path('api/summarizer/', include('apps.summarizer.urls')),
-    path('api/transcriber/', include('apps.transcriber.urls')),
-    path('api/users/', include('apps.users.urls'))
+    path('api/notes/', include('apps.notes.urls'), name='notes'),
+    path('api/summarizer/', include('apps.summarizer.urls'), name='summarizer'),
+    path('api/transcriber/', include('apps.transcriber.urls'), name='transcriber'),
+    path('api/users/', include('apps.users.urls'), name='users'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
