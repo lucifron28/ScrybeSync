22e0f85c6b3021addf2b237e6f4660e51c47eabe
ron <lucifron28@gmail.com>
2025-07-25 00:54:34 +0800

    feat: update user authentication URLs to use custom views

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .views import RegisterView
+from .views import RegisterView, LoginView, RefreshTokenView, LogoutView
+
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('token/refresh/', RefreshTokenView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
