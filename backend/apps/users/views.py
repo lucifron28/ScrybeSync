1e7d13355d90b84463415e59c4e34c324aff1785
ron <lucifron28@gmail.com>
2025-07-24 15:41:45 +0800

    modify the register view to use the custom user model instead of the default

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from django.contrib.auth import get_user_model
+from .serializers import UserRegistrationSerializer, UserSerializer 
+
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
+    queryset = get_user_model().objects.all()
+    serializer_class = UserRegistrationSerializer
     permission_classes = [permissions.AllowAny]
