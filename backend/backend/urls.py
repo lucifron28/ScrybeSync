93f5c9229b217ef7905957c439c297acbe4d2037
ron <lucifron28@gmail.com>
2025-07-24 15:42:04 +0800

    move the auth related endpoints to users

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +7,4 @@ urlpatterns = [
     path('api/summarizer/', include('apps.summarizer.urls'), name='summarizer'),
     path('api/transcriber/', include('apps.transcriber.urls'), name='transcriber'),
     path('api/users/', include('apps.users.urls'), name='users'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
