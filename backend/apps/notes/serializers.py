ccd03cdd694ebc9935227b5b3571a14deb05690f
ron <lucifron28@gmail.com>
2025-07-24 16:04:11 +0800

    create CategorySerializer

---
+++ backend/apps/notes/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Category, Note
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'color', 'created_at']
+        read_only_fields = ['created_at']
+
